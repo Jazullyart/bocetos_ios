dfd4aa24bcc41a11b8cd4253091b9d15d88f07b7
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2025-03-28 10:40:22 -0600

    [AGREGA] una forma de ver la información del usuario que hizo la publicación

--- redes_sociales/redes_sociales/ui/publicaciones/publicacion.swift
+++ redes_sociales/redes_sociales/ui/publicaciones/publicacion.swift
@@ -13,14 +13,16 @@ struct PublicacionVista: View {
     @Environment(ControladorAplicacion.self) var controlador
     
     var body: some View {
-        Text("\(controlador.publicacion_seleccionada?.title)")
-        Text("\(controlador.publicacion_seleccionada?.body)")
+        Text("\(controlador.publicacion_seleccionada?.title ?? "Valor por defecto")")
+        Text("\(controlador.publicacion_seleccionada?.body ?? "Valor por defecto")")
         
         NavigationLink{
-            Text("Hola mundo")
+            PerfilBasicoVista()
         } label: {
             Text("Ver perfil")
-        }
+        }.simultaneousGesture(TapGesture().onEnded({
+            controlador.ver_perfil(id: controlador.publicacion_seleccionada!.userId)
+        }))
         
         ScrollView{
             VStack{
