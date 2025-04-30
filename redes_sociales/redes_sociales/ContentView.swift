0efb1acdd5476e72d96890c17e41023c78e460da
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2025-04-30 10:09:05 -0600

    [AGREGA] Añadir foto de perifl

--- redes_sociales/redes_sociales/ContentView.swift
+++ redes_sociales/redes_sociales/ContentView.swift
@@ -6,9 +6,29 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var foto_seleccionada: PhotosPickerItem? = nil
+    @State var foto_a_mostrar: UIImage? = nil
     var body: some View {
+        PhotosPicker(selection: $foto_seleccionada){
+            Image(uiImage: foto_a_mostrar ?? UIImage(resource: .avatar))
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                .clipShape(.circle)
+        }
+        .onChange(of: foto_seleccionada){valor_anterior, valor_nuevo in
+            Task{
+                if let foto_seleccionada, let datos = try? await foto_seleccionada.loadTransferable(type: Data.self){
+                    if let image = UIImage(data: datos){
+                        foto_a_mostrar = image
+                    }
+                }
+            }
+        }
+        
         GeneralPublicaciones()
     }
 }
